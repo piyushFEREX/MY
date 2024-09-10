6cee01b31ae2d6e55e0e3aabdea318ed7eaa7c08
piyushFEREX <piyushkumarktr@gmail.com>
2024-09-10 20:06:22 +0530

    permormance improvemnrts

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,9 +5,11 @@ const NavBar = () => {
     const{MobileScreen,theme,settheme}=useContext(MyContext)
   return (
     <div
-    style={{width : 'calc(100vw - (100vh * 1.62)'}}
+    style={{
+      minWidth: 'calc(100vw - (100vh * 1.62))',
+    }}    
     className='h-full p-2 z-20 absolute right-0 top-0'>
-<div className='h-full w-full glassmorphism rounded-2xl'>
+    <div className='h-full w-full glassmorphism rounded-2xl'>
     <button className='p-2 bg-red-600 rounded-full' onClick={()=>settheme(!theme)}> theme </button>
 
     </div>   
